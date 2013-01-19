2fb5fe0002ebf7aaea388548d2f1ef9186c6c062
Syl Turner <syl@sylturner.com>
2013-01-19 12:05:29 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Disocial::Application.config.secret_token = '79e1cd2ed5c0f698f0aec4bc5172daa67c3c309065ce4b80cb8e06bc0b28b55bdebe5ac2b0cb3f0fa73fc9e8dee2b5cbeb3ef4273bf254524ee231dc5f4d99c0'
